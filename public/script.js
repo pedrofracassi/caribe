42ac94546e0a84b2789b8da30dc3047e8f3c1d04
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-09-11 19:34:56 -0300

    Pirateando

--- public/script.js
+++ public/script.js
@@ -1,2 +1,6 @@
-var audio = new Audio('pirates.mp3');
-audio.play();
+function startDownload() {
+  var audio = new Audio('pirates.mp3');
+  audio.play();
+  $('#btn-start').html('<i class="fa fa-spinner fa-spin"></i> Pirateando');
+  $('#btn-start').attr('disabled', 'true');
+}
