a171327ada276fc7a655668d9396ca15fe1aa89d
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-09-11 23:32:58 -0300

    Almost done

--- index.js
+++ index.js
@@ -1,19 +1,76 @@
 const path = require('path');
 const express = require('express');
 const WebSocket = require('ws');
+const fs = require('fs');
+const youtubedl = require('youtube-dl');
+var ffmpeg = require('ffmpeg');
 
 const app = express();
 const wss = new WebSocket.Server({ port: 9090 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    if (message.type == 'startDownload') {
-      console.log(message.url);
+    var json = JSON.parse(message);
+    if (json.type == 'startDownload') {
+      var video = youtubedl(json.url, ['--format=18'], { cwd: __dirname });
+
+      // Will be called when the download starts.
+      video.on('info', function(info) {
+        video.__info__ = info; // Yes, I'm injecting stuff into the video object. The famous Brazilian Gambiarra
+        console.log('Download started');
+        console.log('filename: ' + info._filename);
+        console.log('size: ' + info.size);
+        video.pipe(fs.createWriteStream(__dirname + '/public/mp4/' + parseFileName(info.fulltitle + '-' + info.display_id) + '.mp4'));
+      });
+
+      // Will be called when the download ends (duh)
+      video.on('end', function complete() {
+        console.log('Download finished!');
+        try {
+          var parsedFileName = video.__info__._filename;
+          console.log('Filename: ' + parsedFileName);
+          var process = new ffmpeg('public/mp4/' + parsedFileName);
+          process.then(function (newvideo) {
+            newvideo.fnExtractSoundToMP3('public/mp3/' + parsedFileName.replace('mp4', 'mp3'), function (error, file) {
+              if (!error) {
+                console.log('Audio file: ' + file);
+                var success = {
+                  type: 'conversionFinished',
+                  downloadUrl: 'mp3/' + parsedFileName.replace('mp4', 'mp3'),
+                  thumbnailUrl: video.__info__.thumbnail,
+                  videoName: video.__info__.fulltitle
+                }
+                ws.send(JSON.stringify(success));
+              } else {
+                var error = {
+                  type: 'error',
+                  message: 'Houve um erro ao converter o vídeo para mp3.' + error
+                }
+                ws.send(JSON.stringify(error));
+              }
+            });
+          }, function (err) {
+            var error = {
+              type: 'error',
+              message: 'Erro ao converter o vídeo: ' + err
+            }
+            ws.send(JSON.stringify(error));
+          });
+        } catch (e) {
+          var error = {
+            type: 'error',
+            message: 'FFMPEG ' + e.code + ': ' + e.msg
+          }
+          ws.send(JSON.stringify(error));
+        }
+      });
     }
   });
-  console.log('New connection!')
-  ws.send('something');
 });
+
+function parseFileName(name) {
+  return name.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+}
 
 var staticPath = path.join(__dirname, '/public');
 var port = process.env.PORT || 8080;
--- public/script.js
+++ public/script.js
@@ -1,9 +1,5 @@
-var exampleSocket = new WebSocket("ws://localhost:9090/");
+var socket = new WebSocket("ws://localhost:9090/");
 var music = new Audio('pirates.mp3');
-
-exampleSocket.onopen = function (event) {
-  exampleSocket.send("Connected");
-};
 
 function startDownload() {
   var url = $('#textbox-url').val();
@@ -13,19 +9,57 @@ function startDownload() {
     $('#div-notyoutube').attr('style', 'display: none');
     $('#textbox-url').attr('disabled', 'true');
     $('#btn-start').attr('disabled', 'true');
-    exampleSocket.send({type: 'startDownload', url: url});
+    var message = {
+      type: 'startDownload',
+      url: url
+    }
+    socket.send(JSON.stringify(message));
     console.log('Válido');
   } else {
+    $('#textbox-url').val('');
     $('#div-notyoutube').attr('style', 'display: block');
     console.log('Nope');
   }
 }
 
-// Thanks to Jitendra Pancholi from StackOverflow for this function
+// Thanks to Jitendra Pancholi @ StackOverflow for this function
 function isYoutubeUrl(url) {
-    var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-    if(url.match(p)){
-        return url.match(p)[1];
-    }
-    return false;
+  var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  if(url.match(p)){
+    return url.match(p)[1];
+  }
+  return false;
 }
+
+
+socket.onmessage = function (event) {
+  var message = JSON.parse(event.data);
+  resetUI();
+  if (message.type == 'conversionFinished') {
+    $('#div-downloaded').append(`<a href="${message.downloadUrl}" download> ${message.videoName} </a>`);
+  } else if (message.type == 'error') {
+    resetUI();
+    alert(message.message);
+  } else {
+    resetUI();
+    alert('Um erro ocorreu.');
+  }
+}
+
+function resetUI() {
+  $('#btn-start').html('<i class="fa fa-download"></i> Piratear');
+  $('#textbox-url').removeAttr('disabled');
+  $('#textbox-url').val('');
+  $('#btn-start').removeAttr('disabled');
+  music.pause();
+}
+/*
+<div class="card" style="width: 20rem;">
+<img class="card-img-top" src="..." alt="Card image cap">
+<div class="card-body">
+<h4 class="card-title">Card title</h4>
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+*/
