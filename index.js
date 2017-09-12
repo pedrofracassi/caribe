6b19e893e447cb0cc23e0a7228d74e4ef648e78d
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-09-11 21:10:13 -0300

    Youtube checks, websockets and more

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-var path = require('path');
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require('express');
+const WebSocket = require('ws');
+
+const app = express();
+const wss = new WebSocket.Server({ port: 9090 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    if (message.type == 'startDownload') {
+      console.log(message.url);
+    }
+  });
+  console.log('New connection!')
+  ws.send('something');
+});
 
 var staticPath = path.join(__dirname, '/public');
 var port = process.env.PORT || 8080;
--- public/script.js
+++ public/script.js
@@ -1,6 +1,31 @@
+var exampleSocket = new WebSocket("ws://localhost:9090/");
+var music = new Audio('pirates.mp3');
+
+exampleSocket.onopen = function (event) {
+  exampleSocket.send("Connected");
+};
+
 function startDownload() {
-  var audio = new Audio('pirates.mp3');
-  audio.play();
-  $('#btn-start').html('<i class="fa fa-spinner fa-spin"></i> Pirateando');
-  $('#btn-start').attr('disabled', 'true');
+  var url = $('#textbox-url').val();
+  if (isYoutubeUrl(url)) {
+    music.play();
+    $('#btn-start').html('<i class="fa fa-spinner fa-spin"></i> Pirateando');
+    $('#div-notyoutube').attr('style', 'display: none');
+    $('#textbox-url').attr('disabled', 'true');
+    $('#btn-start').attr('disabled', 'true');
+    exampleSocket.send({type: 'startDownload', url: url});
+    console.log('Válido');
+  } else {
+    $('#div-notyoutube').attr('style', 'display: block');
+    console.log('Nope');
+  }
+}
+
+// Thanks to Jitendra Pancholi from StackOverflow for this function
+function isYoutubeUrl(url) {
+    var p = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+    if(url.match(p)){
+        return url.match(p)[1];
+    }
+    return false;
 }
